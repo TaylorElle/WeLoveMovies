f8280c27d2ef48e7572f23fb158b4f3793dcf57f
“Taylor <duggan.taylor24@yahoo.com>
2021-09-23 15:06:22 -0500

    update to reviews controller and service

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -4,35 +4,36 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 ///middleware fxn that checks if reviewId exists
 async function reviewExists(req, res, next) {
   const { reviewId } = req.params;
+  console.log(req);
+  console.log(req.params);
   const review = await service.read(reviewId);
   if (review) {
     res.locals.review = review;
-    console.log(res.locals.review);
     return next();
   }
-  console.log("review not found");
-  return next({ status: 404, error: "Review cannot be found" });
-}
-
-async function update(req, res) {
-  //should return a 404 if the ID given does not match any ID in the database
-  console.log(req);
-  const updatedReview = {
-    ...req.body.data,
-  };
-  console.log(updatedReview);
-  const data = await service.update(updatedReview);
-  console.log(data);
-  res.json({ data });
+  return next({ status: 404, message: "Review cannot be found" });
 }
 
 async function destroy(req, res) {
   const reviewId = res.locals.review.review_id;
-  const data = await service.destroy(reviewId);
-  res.sendStatus(204).json({ data: data });
+  await service.destroy(reviewId);
+  res.sendStatus(204).json("No content");
+}
+
+async function update(req, res) {
+  console.log("req.body", req.body);
+  const updatedReview = {
+    ...res.locals.review,
+    ...req.body,
+    review_id: res.locals.review.review_id,
+  };
+  const criticsInfo = await service.update(updatedReview);
+  updatedReview.critic = criticsInfo;
+  res.json({ data: updatedReview });
 }
 
 module.exports = {
-  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
   delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
 };
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,49 +1,27 @@
 const knex = require("../db/connection");
-const mapProperties = require("../utils/map-properties");
 
-const addCritic = mapProperties({
-  critic_id: "critic.critic_id",
-  preferred_name: "critic.preferred_name",
-  surname: "critic.surname",
-  organization_name: "critic.organization_name",
-});
+function destroy(reviewId) {
+  return knex("reviews").where({ review_id: reviewId }).del();
+}
 
 function read(reviewId) {
-  return knex("reviews")
-    .select("*")
-    .where({ review_id: reviewId })
-    .groupBy("review_id")
-    .first();
+  return knex("reviews").where({ review_id: reviewId }).first();
 }
 
-//put:
 function update(updatedReview) {
-  return knex("reviews as r")
-    .join("critics as c", "c.critic_id", "r.critic_id")
-    .where({ "r.review_id": updatedReview.review_id })
-    .groupBy("critic_id")
-    .select("r.*", "c.*")
+  return knex("reviews")
+    .where({ review_id: updatedReview.review_id })
     .update(updatedReview, "*")
-    .then((results) => {
-      results.forEach((oneResult) => {
-        const criticArr = [];
-        const addingACritic = addCritic(oneResult);
-        criticArr.push(addingACritic);
-      });
-      return criticArr;
+    .then(() => {
+      return knex("critics")
+        .select("preferred_name", "surname", "organization_name")
+        .where({ critic_id: updatedReview.critic_id })
+        .first();
     });
 }
-//updates an existing review, returning the updated review including the critic info
-
-//destroy
-function destroy(reviewId) {
-  return knex("reviews as r").select("*").where({ review_id: reviewId }).del();
-}
-//should return a 404 if the ID given does not match any ID in the database
-//should delete the review record when given an existing review_id
 
 module.exports = {
+  destroy,
   read,
   update,
-  destroy,
 };
